768a81ef20a88c9de0538ef719b820bccf3410bd
Zhenzhi <caizhenzhi5@gmail.com>
2025-01-18 22:16:43 +0800

    Spawn enemies

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     private Vector3 stepAmount = new Vector3(-1, 0, 0);
+    public int damage = 1;
+    public float lastInstance;
+    public float damageInstance = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-            
+        lastInstance = Time.realtimeSinceStartup;
     }
 
     // Update is called once per frame
     void Update() 
     { 
         gameObject.transform.position = gameObject.transform.position + (stepAmount * Time.deltaTime);
+        if (stepAmount == Vector3.zero && Time.realtimeSinceStartup - lastInstance > damageInstance)
+        {
+            GameManager.instance.SetWallHp(GameManager.instance.GetWallHp() - damage);
+            lastInstance = Time.realtimeSinceStartup;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject goblin;
+    private BoxCollider2D box;
+    private float low;
+    private float high;
+
+    void Start()
+    {
+        box = gameObject.GetComponent<BoxCollider2D>();
+        low = box.bounds.center.y - box.bounds.size.y/2;
+        high = box.bounds.center.y + box.bounds.size.y/2;
+        Debug.Log(low);
+        Debug.Log(high);
+        Debug.Log(box.bounds.center.y);
+        Debug.Log(box.bounds.size.y / 2);
+        Debug.Log(box.bounds.size.y);
+    }
+
+    public void spawn()
+    {
+        Instantiate(goblin, new Vector3(box.bounds.center.x, Random.Range(low, high), box.bounds.center.z), goblin.transform.rotation);
+    }
+}
